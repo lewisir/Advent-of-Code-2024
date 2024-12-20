e72b18a4d31d91d26e428ed82175fdf3d3894487
Ian Lewis <ian.r.lewis@bt.com>
2024-12-20 12:33:29 +0000

    Day 7 Part I solution

--- Day7/aoc_day7.py
+++ Day7/aoc_day7.py
@@ -1,6 +1,6 @@
 """
 --- Advent of Code 2024 ---
---- Day 7: xxxxxxxxxxxx ---
+--- Day 7: Bridge Repair ---
 https://adventofcode.com/2024/day/7
 """
 
@@ -21,6 +21,47 @@ else:
 def main():
     """Main program"""
     data = get_input_data(FILENAME)
+    calibration_data = process_data(data)
+    result_sum = 0
+    for equation in calibration_data:
+        target, sequence = equation
+        if perm_calculation(target, sequence):
+            result_sum += target
+    print(f"Part I Result Sum {result_sum}")
+
+
+def perm_calculation(target, sequence):
+    """Output all of the possible addition and multiplications of the sequence"""
+    permutations = 2 ** (len(sequence) - 1)
+    for perm in range(permutations):
+        calc_result = sequence[0]
+        for bit in range(len(sequence) - 1):
+            if 2**bit & perm == 0:
+                calc_result += sequence[bit + 1]
+            elif 2**bit & perm != 0:
+                calc_result *= sequence[bit + 1]
+            if calc_result > target:
+                continue
+        if calc_result == target:
+            return True
+    return False
+
+
+def process_data(data):
+    "Process the input data line by line"
+    result_list = []
+    for line in data:
+        result_list.append(process_line(line))
+    return result_list
+
+
+def process_line(line):
+    """Process the line to return the result and the sequence of integers"""
+    temp = line.split(":")
+    result = int(temp[0])
+    sequence = temp[1].split()
+    seq = [int(x) for x in sequence]
+    return result, seq
 
 
 def get_input_data(filename):
