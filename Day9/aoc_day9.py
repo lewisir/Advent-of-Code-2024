917182525ffc9b528ff339d629086886b9c65c39
lewisir <ian.r.lewis@bt.com>
2025-01-01 12:00:26 +0000

    Day 9 Part II solved

--- Day9/aoc_day9.py
+++ Day9/aoc_day9.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2024/day/9
 
 from time import perf_counter
 
-TEST = False
+TEST = True
 
 DAY = "9"
 REAL_INPUT = "Advent-of-Code-2024/Day" + DAY + "/input_day" + DAY + ".txt"
@@ -21,10 +21,17 @@ else:
 def main():
     """Main program"""
     data = get_input_data(FILENAME)
-    small_disk_test = "12345"
     disk_blocks = make_disk_blocks(data[0])
     disk_blocks = defrag(disk_blocks)
     print(f"Part I Checksum = {calcualate_checksum(disk_blocks)}")
+
+    disk_blocks = make_disk_blocks(data[0])
+    file_blocks = get_file_blocks(data[0])
+    free_blocks = get_free_blocks(data[0])
+    disk_blocks = defrag_by_file(file_blocks, free_blocks, disk_blocks)
+    
+    print(f"Part II Checksum = {calcualate_checksum(disk_blocks)}")
+
 
 def calcualate_checksum(disk_blocks):
     """Return the calcualated checksum"""
@@ -33,6 +40,35 @@ def calcualate_checksum(disk_blocks):
         if type(value) is int:
             checksum += index*value
     return checksum
+
+
+def defrag_by_file(file_blocks,free_blocks,disk_blocks):
+    """defrag the disk by moving whole files"""
+    for file_id in range(len(file_blocks)-1,0,-1):
+        file_length = file_blocks[file_id][1]
+        free_space_position = find_free_space(free_blocks, file_length)
+        if free_space_position and free_space_position < file_blocks[file_id][0]:
+            free_blocks,disk_blocks = move_file(file_id,file_blocks[file_id][0], file_blocks[file_id][1], free_space_position, free_blocks, disk_blocks)
+    return disk_blocks
+
+def move_file(file_id, file_position, file_size, destination, free_blocks, disk_blocks):
+    """Move the file from to the new position"""
+    for i in range(file_size):
+        disk_blocks[destination+i] = file_id
+        disk_blocks[file_position+i] = '.'
+    for index,space in enumerate(free_blocks):
+        if space[0] == destination:
+            free_blocks[index] = (destination+file_size,space[1]-file_size)
+    return free_blocks,disk_blocks
+
+
+
+def find_free_space(free_blocks, block_length):
+    """return the position of the first free block what is at least as big as the block_length. Else return None"""
+    for space in free_blocks:
+        if space[1] >= block_length:
+            return space[0]
+    return None
 
 def defrag(disk_blocks):
     """Defragment the disk moving file ids from the end to the first free space"""
@@ -43,7 +79,7 @@ def defrag(disk_blocks):
         disk_blocks[last_file_block] = '.'
         first_free_block = disk_blocks.index('.')
         last_file_block = find_last_file_block(disk_blocks)
-        print(f"{100*first_free_block/last_file_block}% Complete")
+        # print(f"{100*first_free_block/last_file_block}% Complete")
     return disk_blocks
 
 def find_last_file_block(disk_blocks):
@@ -52,39 +88,10 @@ def find_last_file_block(disk_blocks):
         if type(disk_blocks[i]) is int:
             return i
 
-
-def extract_disk_blocks(disk_map):
-    """Given the disk map return a dictionary of the file IDs and their blocks and a list of the free blocks"""
-    file_id = 0
-    file_blocks = {}
-    free_blocks = []
-    position = 0
-    for digit in range(len(disk_map)):
-        if digit % 2 == 0:
-            file_blocks[file_id] = (position, int(disk_map[digit]))
-            file_id += 1
-        else:
-            free_blocks.append((position, int(disk_map[digit])))
-        position += int(disk_map[digit])
-    return file_blocks, free_blocks
-
-
-def display_disk_from_blocks(file_blocks):
-    """Print a representation of the files on the disk from the disk block"""
-    output_string = ""
-    for file_id in range(len(file_blocks)):
-        if file_blocks[file_id][0] > len(output_string):
-            output_string += "." * (file_blocks[file_id][0] - len(output_string))
-            output_string += str(file_id) * file_blocks[file_id][1]
-        else:
-            output_string += str(file_id) * file_blocks[file_id][1]
-    print(output_string)
-
 def make_disk_blocks(disk_map):
-    """produce a list of the disk blocks from the disk map showing file IDs nd free space"""
+    """produce a list of the disk blocks from the disk map showing file IDs and free space"""
     disk_blocks = []
     file_id = 0
-    position = 0
     for digit in range(len(disk_map)):
         if digit % 2 == 0:
             for blocks in range(int(disk_map[digit])):
@@ -94,6 +101,30 @@ def make_disk_blocks(disk_map):
             for blocks in range(int(disk_map[digit])):
                 disk_blocks.append('.')
     return disk_blocks
+
+
+def get_file_blocks(disk_map):
+    """return a dictionary of file IDs and their block locations"""
+    file_blocks = {}
+    file_id = 0
+    position = 0
+    for digit in range(len(disk_map)):
+        if digit % 2 == 0:
+            file_blocks[file_id] = (position,int(disk_map[digit]))
+            file_id += 1
+        position += int(disk_map[digit])
+    return file_blocks
+
+def get_free_blocks(disk_map):
+    """return a list of the free blocks"""
+    free_blocks = []
+    position = 0
+    for digit in range(len(disk_map)):
+        if digit % 2 == 1:
+            free_blocks.append((position,int(disk_map[digit])))
+        position += int(disk_map[digit])
+    return free_blocks
+
 
 def get_input_data(filename):
     """function to read in the input data"""
